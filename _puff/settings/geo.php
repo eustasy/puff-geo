c313d349f8295290b93e335245b042e7cb7990a0
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-14 10:31:14 +0000

    Create geo.php

---
+++ _puff/settings/geo.php
@@ -0,0 +1,9 @@
+<?php
+
+////	Settings
+//
+// Do you want to geo-locate on every page load?
+$Sitewide['Settings']['GeoLocate On Load'] = true;
+
+// Geo Version
+$Sitewide['Version']['Geo'] = '0.1';
